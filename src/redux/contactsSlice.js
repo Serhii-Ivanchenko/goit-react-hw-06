a6af4fb38d58ed5c981928992e080b82bfa5ba3b
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-06-15 18:04:03 +0300

    create contactsSlice using createSlice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,20 +1,20 @@
 import { initialState } from './initialState';
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const addContact = createAction('contacts/addContact');
 
-export const deleteContact = createAction('contacts/deleteContact');
+const slice = createSlice({
+  name: 'contacts',
+  initialState: initialState.contacts,
+  reducers: {
+    addContact(state, action) {
+      state.items.push(action.payload);
+    },
+    deleteContact(state, action) {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    },
+  },
+});
 
-export const contactsReducer = createReducer(
-  initialState.contacts,
-  builder => {
-    builder
-      .addCase(addContact, (state, action) => {
-        state.items.push(action.payload);
-      })
-      .addCase(deleteContact, (state, action) => {
-        state.items = state.items.filter(item => item.id !== action.payload);
-      });
-  }
-);
+export const { addContact, deleteContact } = slice.actions;
 
+export default slice.reducer;
